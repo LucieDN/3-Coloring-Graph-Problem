29e50510dc613e7049b9731e95205b983940f7dd
Lucie Della Negra <ldellanegra@ensc.fr>
2026-02-09 13:32:34 +0100

    update: created specific python file for plotting graph in index

---
+++ plot_graph.py
@@ -0,0 +1,34 @@
+import ast
+import networkx as nx
+import matplotlib.pyplot as plt
+from js import document
+
+seed = global_vars["seed"]
+
+# Safely parse Python literals
+edges = ast.literal_eval(edges_str)
+
+# Use Networkx to build corresponding graph
+G = nx.Graph()
+G.add_edges_from(edges)
+pos = nx.spring_layout(G, seed=seed)
+
+# Draw corresponding graph
+plt.figure(figsize=(8,4), dpi=120)
+nx.draw(G, pos=pos, with_labels = True, node_color='white', node_size=800) # Uncolored initial graph
+
+# Render to browser
+import io, base64
+buf = io.BytesIO()
+plt.savefig(buf, format="png")
+plt.close()
+buf.seek(0)
+
+img_base64 = base64.b64encode(buf.read()).decode("utf-8")
+img = document.createElement("img")
+img.src = "data:image/png;base64," + img_base64
+
+# Clear previous output
+document.getElementById("output").innerHTML = ""
+# Show new plot
+document.getElementById("output").appendChild(img)
