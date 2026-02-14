4ea0cf04527695eb672daf2edf16e0185ff5cc1f
Lucie Della Negra <ldellanegra@ensc.fr>
2026-02-14 10:52:14 +0100

    update: manage impossible coloring case with a dialog box, seperated js and python code in colorGraph part

--- assets/scripts/colorGraph.js
+++ assets/scripts/colorGraph.js
@@ -1,6 +1,35 @@
 // Python execution - color current graph
 async function colorGraph() {
   // Run python code that color current graph
-  pyodide.runPython(await (await fetch("assets/scripts/color_graph.py")).text());
+  let result = pyodide.runPython(await (await fetch("assets/scripts/color_graph.py")).text());
   
+  if (result === "IMPOSSIBLE COLORING") {
+    const dialog = document.createElement("dialog");
+
+    dialog.innerHTML = `
+      <h2>Sorry, can't do it :/</h2>
+      <p>There is no way we can color it with only 3 colors!</p>
+      <button id="closeDialog">I understand and will never ask for it again.</button>
+    `;
+
+    document.body.appendChild(dialog);
+
+    // Show as modal
+    dialog.showModal();
+
+    // Close button logic
+    dialog.querySelector("#closeDialog").addEventListener("click", () => {
+        dialog.close();
+        dialog.remove();
+});
+  }
+  else{
+    img = document.createElement("img")
+    img.src = "data:image/png;base64," + result
+
+    // Clear previous output
+    document.getElementById("output").innerHTML = ""
+    // Show new plot
+    document.getElementById("output").appendChild(img)
+  }
 }
--- assets/scripts/setup_config.py
+++ assets/scripts/setup_config.py
@@ -2,7 +2,7 @@ import ast
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-from js import document
+from js import document, alert
 
 seed = global_vars["seed"]
 colors = global_vars["colors"]
